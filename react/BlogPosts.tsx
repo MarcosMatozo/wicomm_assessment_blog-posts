6c8bd256238a90136e0882135d161a45534ca3ce
MarcosMatozo <marcoshenriquesp@hotmail.com>
2025-09-08 22:44:22 -0300

    first commit

--- react/BlogPosts.tsx
+++ react/BlogPosts.tsx
@@ -4,8 +4,8 @@ type Props = {
   name: string
 }
 
-function Greeting({ name }: Props) {
+function BlogPosts({ name }: Props) {
   return <div>Hey, {name}</div>
 }
 
-export default Greeting
+export default BlogPosts
